badc4bf103107b36d762945dcc478f3e3248ba75
saleehberlin <berlinsaleeh@gmail.com>
2025-04-17 15:35:12 +0300

    Update views.py

--- aid/views.py
+++ aid/views.py
@@ -12,22 +12,26 @@ def aid_objects(request):
 
 
 
-'''def aid_objects(request):
-    aids = AidRequest.objects.all()
+def donating(request):
+    donors = donors.objects.all()
     template = loader.get_template('Aidobjects.html')
     context = {
-        'aids': aids,  
+        'donors': donors,  
     }
-    return HttpResponse(template.render(context, request))'''
+    return HttpResponse(template.render(context, request))
 
 
 
 from rest_framework import generics
-from .serializers import AidRequestSerializer
+from .serializers import AidRequestSerializer,donorsSerializer
 
 class AidRequestCreateView(generics.CreateAPIView):
     queryset = AidRequest.objects.all()
     serializer_class = AidRequestSerializer
+
+class donorsCreateView(generics.CreateAPIView):
+    queryset = donors.objects.all()
+    serializer_class = donorsSerializer
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
@@ -38,4 +42,11 @@ class AidRequestListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['fullname']  
     search_fields = ['fullname', 'email', 'country']
+
+class donorsListView(generics.ListAPIView):
+    queryset = donors.objects.all()
+    serializer_class = donorsSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['brandname']  
+    search_fields = ['brandname', 'donated_at', 'distributed']
 
