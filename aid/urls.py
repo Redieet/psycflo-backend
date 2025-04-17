f050a718f63189e7c481f57b9c9e4bdeb47396c8
saleehberlin <berlinsaleeh@gmail.com>
2025-04-17 15:38:06 +0300

    Update urls.py

--- aid/urls.py
+++ aid/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import AidRequestCreateView, AidRequestListView
+from .views import AidRequestCreateView, AidRequestListView, donorsCreateView, donorsListView
 
 urlpatterns = [
     path('', AidRequestListView.as_view(), name='aid-list'),
     path('create/', AidRequestCreateView.as_view(), name='aid-create'),
     path('list/', AidRequestListView.as_view(), name='aid-list'),
+
+    path('', donorsListView.as_view(), name='donors-list'),
+    path('create/',donorsCreateView.as_view(), name='donors-create'),
+    path('list/', donorsListView.as_view(), name='donors-list'),
 ]
