4604a7c623576447f6aac739e55b90e1fae55876
saleehberlin <berlinsaleeh@gmail.com>
2025-04-17 15:27:43 +0300

    Update admin.py

--- aid/admin.py
+++ aid/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from.models import AidRequest
+from.models import AidRequest,donors
 # Register your models here.
 admin.site.register(AidRequest)
+admin.site.register(donors)
 class MemberAdmin(admin.ModelAdmin):
   list_display = ("brandname",  "donated_at",)
 
