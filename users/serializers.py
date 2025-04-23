7b5fd32e5d98cd988457becc115b9e49129dce53
rediet <148014485+Redieet@users.noreply.github.com>
2025-04-22 17:22:41 -0700

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-# Registration Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -9,5 +8,5 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        return User.objects.create_user(**validated_data)
+
